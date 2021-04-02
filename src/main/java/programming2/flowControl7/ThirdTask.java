4184824ecd2cb2af535daceee2dcda82f90bb15c
Serhii <serg.ua92@mail.ru>
2021-04-02 18:55:28 +0300

    - fixed 3 flow control tasks

--- src/main/java/programming2/flowControl7/ThirdTask.java
+++ src/main/java/programming2/flowControl7/ThirdTask.java
@@ -1,7 +1,5 @@
 package programming2.flowControl7;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class ThirdTask {
@@ -11,29 +9,26 @@ public class ThirdTask {
 
     public static void initializeArray() {
         Scanner input = new Scanner(System.in);
-//        Integer[] list = new Integer[3];
+        int[] list = new int[3];
         System.out.println("Input 3 number: ");
-        int a = input.nextInt();
-        int b = input.nextInt();
-        int c = input.nextInt();
-        if (a >= b && b >= c) {
-            System.out.println(a + " " + b + " " + c);
-        } else if (a >= c && c >= b) {
-            System.out.println(a + " " + c + " " + b);
-        } else if (c >= a && a >= b) {
-            System.out.println(c + " " + a + " " + b);
-        } else if (c >= b && b >= a) {
-            System.out.println(c + " " + b + " " + a);
-        } else if (b >= a && a >= c) {
-            System.out.println(b + " " + a + " " + c);
-        } else if (b >= c && c >= a) {
-            System.out.println(b + " " + c + " " + a);
+        for (int i = 0; i < list.length; i++) {
+            list[i] = input.nextInt();
         }
-        //        for (int i = 0; i < list.length; i++) {
-//            list[i] = input.nextInt();
-//        }
-//        Arrays.sort(list, Collections.reverseOrder());
-//        System.out.println(Arrays.toString(list));
+        sort(list);
+        for (int j : list) {
+            System.out.println(j + " ");
+        }
     }
 
+    public static void sort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = array.length - 1; j > i; j--) {
+                if (array[j - 1] < array[j]) {
+                    int tmp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = tmp;
+                }
+            }
+        }
+    }
 }
